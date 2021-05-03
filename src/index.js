9ff39c82959418dc04aa9c74163e2922bb0f235e
Pär Sjöberg <paer.sjoberg@googlemail.com>
2021-05-03 06:06:29 +0200

    App removed and references changed to Docs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Docs from './Dcos';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Docs />
   </React.StrictMode>,
   document.getElementById('root')
 );
